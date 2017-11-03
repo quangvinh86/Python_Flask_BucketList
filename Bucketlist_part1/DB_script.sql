a3780860a7347b18d7957a643c60a918d1d686a9
quangvinh86 <quangvinh19862003@gmail.com>
2017-11-03 10:59:08 +0700

    Bucketlist_part1

---
+++ Bucketlist_part1/DB_script.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE BucketList;
+
+/*Create table tbl_user*/
+CREATE TABLE `BucketList`.`tbl_user` (
+  `user_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NULL,
+  `user_username` VARCHAR(45) NULL,
+  `user_password` VARCHAR(45) NULL,
+  PRIMARY KEY (`user_id`));
+
+
+/*Create store procedure sp_createUser*/
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createUser`(
+    IN p_name VARCHAR(45),
+    IN p_username VARCHAR(45),
+    IN p_password VARCHAR(45)
+)
+BEGIN
+    if ( select exists (select 1 from tbl_user where user_username = p_username) ) THEN
+     
+        select 'Username Exists !!';
+     
+    ELSE
+     
+        insert into tbl_user
+        (
+            user_name,
+            user_username,
+            user_password
+        )
+        values
+        (
+            p_name,
+            p_username,
+            p_password
+        );
+     
+    END IF;
+END
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Python_Flask_BucketList
 example project with Python Flask and MySQL
+Based on: https://code.tutsplus.com/series/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-827
+
+Updated by quangvinh86:
+Part 1: 
+- Move config information to app.ini
+- Init DB on first session.
+- Move content of signup.js file to signup.html (because jquery 3 didn't support)
+
